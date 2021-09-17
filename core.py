6cf0bc90ffba866a2a8abd9f176a53e1c403b733
spoons <childofunix@gmail.com>
2021-09-17 16:57:14 -0400

    fix: style code

--- core.py
+++ core.py
@@ -13,6 +13,7 @@ from utils.processes import execute
 class InvalidSelectionException(Exception):
     pass
 
+
 class Mod:
     def __init__(self, name, steamid, versions, author, fp):
         self.name = name
@@ -20,9 +21,11 @@ class Mod:
         self.author = author
         self.steamid = steamid
         self.fp = fp
+
     def __repr__(self):
-        return "<Mod name:{} author:{} steamid:{} versions:{}>"\
-            .format(self.name, self.author, self.steamid, self.versions)
+        return "<Mod name:{} author:{} steamid:{} versions:{}>".format(
+            self.name, self.author, self.steamid, self.versions
+        )
 
     def __cell__(self):
         return [self.name, self.author, self.steamid, self.versions]
@@ -41,10 +44,10 @@ class Mod:
     def create_from_path(cls, filepath):
         tree = ET.parse(filepath + "/About/About.xml")
         root = tree.getroot()
-        name = root.find('name').text
-        author = root.find('author').text
-        versions = [v.text for v in root.find('supportedVersions').findall('li')]
-        with open(filepath+"/About/PublishedFileId.txt") as f:
+        name = root.find("name").text
+        author = root.find("author").text
+        versions = [v.text for v in root.find("supportedVersions").findall("li")]
+        with open(filepath + "/About/PublishedFileId.txt") as f:
             steamid = f.readline().strip()
 
         return Mod(name, steamid, versions, author, filepath)
@@ -74,51 +77,66 @@ class ModList:
     def write_text_modlist(cls, mods, path):
         with open(path, "w") as f:
             f.write(cls.export_text_modlist(mods))
-              
+
 
 class SteamDownloader:
     @classmethod
     def download(cls, mods, folder):
         def workshop_format(mods):
-            return (s := ' +workshop_download_item 294100 ') + s.join(str(x) for x in mods)
-        query = "steamcmd +login anonymous +force_install_dir \"{}\" \"{}\" +quit"\
-          .format(folder, workshop_format(mods))
+            return (s := " +workshop_download_item 294100 ") + s.join(
+                str(x) for x in mods
+            )
+
+        query = 'steamcmd +login anonymous +force_install_dir "{}" "{}" +quit'.format(
+            folder, workshop_format(mods)
+        )
 
         for line in execute(query):
-           print(line, end='') 
+            print(line, end="")
 
     @classmethod
     def download_modlist(cls, mods, path):
         steamids = [n.steamid for n in mods]
         return cls.download(steamids, path)
 
+
 class WorkshopResultsEnum(Enum):
-    TITLE=0
-    AUTHOR=1
-    STEAMID=2
+    TITLE = 0
+    AUTHOR = 1
+    STEAMID = 2
+
 
 class WorkshopWebScraper:
     @classmethod
     def scrape_update_date(cls, mod):
-        resp = req.get("https://steamcommunity.com/sharedfiles/filedetails/?id={}".format(mod.steamid))
-        soup = BeautifulSoup(resp.content, 'html.parser')
-        return list(soup.find_all('div', class_='detailsStatRight'))[2].get_text()
+        resp = req.get(
+            "https://steamcommunity.com/sharedfiles/filedetails/?id={}".format(
+                mod.steamid
+            )
+        )
+        soup = BeautifulSoup(resp.content, "html.parser")
+        return list(soup.find_all("div", class_="detailsStatRight"))[2].get_text()
 
     @classmethod
     def workshop_search(cls, name):
-        name = name.replace(' ', '+')
-        resp = req.get("https://steamcommunity.com/workshop/browse/?appid=294100&searchtext={}".format(name))
-        soup = BeautifulSoup(resp.content, 'html.parser')
-        items = soup.find_all('div', class_='workshopItem')
+        name = name.replace(" ", "+")
+        resp = req.get(
+            "https://steamcommunity.com/workshop/browse/?appid=294100&searchtext={}".format(
+                name
+            )
+        )
+        soup = BeautifulSoup(resp.content, "html.parser")
+        items = soup.find_all("div", class_="workshopItem")
         results = []
         import re
-        for n in items:
-            item_title = n.find('div', class_='workshopItemTitle').get_text()
-            author_name = n.find('div', class_='workshopItemAuthorName').get_text()
-            steamid = int(re.search(r'\d+', n.find('a', class_='ugc')['href']).group())
-            results.append((item_title, author_name, steamid))
 
+        for n in items:
+            item_title = n.find("div", class_="workshopItemTitle").get_text()
+            author_name = n.find("div", class_="workshopItemAuthorName").get_text()
+            steamid = int(re.search(r"\d+", n.find("a", class_="ugc")["href"]).group())
+            results.append((item_title, author_name, steamid))
         return results
+
 
 class Manager:
     def __init__(self, moddir):
@@ -127,19 +145,23 @@ class Manager:
         self.cache_content_dir = self.cachedir + "/steamapps/workshop/content/294100/"
 
     def get_mods_list(self):
-        return [ Mod.create_from_path(self.moddir + "/" + d) for d in os.listdir(self.moddir) ]
+        return [
+            Mod.create_from_path(self.moddir + "/" + d) for d in os.listdir(self.moddir)
+        ]
 
     def modlist_from_list_cache(self, mods):
-        return [ Mod.create_from_path(self.cache_content_dir + "/" + str(d)) for d in mods ]
-    
+        return [
+            Mod.create_from_path(self.cache_content_dir + "/" + str(d)) for d in mods
+        ]
+
     def get_mods_names(self):
         mods = self.get_mods_list()
-        return [ n.name for n in mods ]
+        return [n.name for n in mods]
 
     def backup_mod_dir(self, tarball_fp):
-        query = "(cd {}; tar -vcaf \"{}\" \".\")".format(self.moddir, tarball_fp)
+        query = '(cd {}; tar -vcaf "{}" ".")'.format(self.moddir, tarball_fp)
         for line in execute(query):
-            print(line, end='')
+            print(line, end="")
 
     def sync_mod(self, steamid):
         SteamDownloader().download([steamid], self.cachedir)
@@ -149,18 +171,17 @@ class Manager:
 
     def sync_mod_list(self, modlist_fp):
         mods = ModList.read_text_modlist(modlist_fp)
-        SteamDownloader().download(mods, self.cachedir) 
+        SteamDownloader().download(mods, self.cachedir)
         mods = self.modlist_from_list_cache(mods)
         print("\n")
         for n in mods:
             print("Installing {}".format(n.name))
             n.update_parent_dir(self.cache_content_dir)
             n.install(self.moddir)
-        
 
     def update_all_mods(self, fp):
         mods = self.get_mods_list()
-        SteamDownloader().download_modlist(mods, self.cachedir) 
+        SteamDownloader().download_modlist(mods, self.cachedir)
         print("\n")
         for n in mods:
             print("Updating {}".format(n.name))
@@ -169,12 +190,15 @@ class Manager:
 
     def get_mod_table(self):
         from tabulate import tabulate
-        return tabulate([ m.__cell__() for m in self.get_mods_list() ])
+
+        return tabulate([m.__cell__() for m in self.get_mods_list()])
+
 
 class CLI:
     def __init__(self):
-        parser = argparse.ArgumentParser(description='Rimworld Mod Manager (RMM)',
-                                         usage='''rmm <command> [<args>]
+        parser = argparse.ArgumentParser(
+            description="Rimworld Mod Manager (RMM)",
+            usage="""rmm <command> [<args>]
 The available commands are:
     list        List installed packages 
     update      Update all packages
@@ -184,59 +208,74 @@ The available commands are:
     export      Saves package library state to a file
     search      Searches the workshop for mod
     
-''')
-        parser.add_argument('command', help='Subcommand to run')
+""",
+        )
+        parser.add_argument("command", help="Subcommand to run")
         # parse_args defaults to [1:] for args, but you need to
         # exclude the rest of the args too, or validation will fail
         args = parser.parse_args(sys.argv[1:2])
 
         try:
-            self.path = os.environ['RMM_MOD']
+            self.path = os.environ["RMM_MOD"]
         except KeyError as err:
-            print("Rimworld mod directory not set.\nPlease set \"RMM_MOD\" variable to the Rimworld mod directory in your shell config.\nexport RMM_MOD=\"~/games/rimworld/game/Mods\"")
+            print(
+                'Rimworld mod directory not set.\nPlease set "RMM_MOD" variable to the Rimworld mod directory in your shell config.\nexport RMM_MOD="~/games/rimworld/game/Mods"'
+            )
             exit(1)
 
-        if (not os.path.isdir(self.path)):
-            print("Mod directory not found. Creating new directory '{}'.".format(self.path))
+        if not os.path.isdir(self.path):
+            print(
+                "Mod directory not found. Creating new directory '{}'.".format(
+                    self.path
+                )
+            )
             os.mkdir(self.path)
 
         if not hasattr(self, args.command):
-            print('Unrecognized command')
+            print("Unrecognized command")
             parser.print_help()
             exit(2)
         # use dispatch pattern to invoke method with same name
         getattr(self, args.command)()
 
     def search(self):
-        parser = argparse.ArgumentParser(description="searches the workshop for specified modname")
+        parser = argparse.ArgumentParser(
+            description="searches the workshop for specified modname"
+        )
         parser.add_argument("modname", help="name of mod")
         args = parser.parse_args(sys.argv[2:])
         results = WorkshopWebScraper.workshop_search(args.modname)
         from tabulate import tabulate
-        print(tabulate(reversed(results)))
 
+        print(tabulate(reversed(results)))
 
     def export(self):
         parser = argparse.ArgumentParser(description="Saves modlist to file.")
-        parser.add_argument("filename", help="filename to write modlist to or specify '-' for stdout")
+        parser.add_argument(
+            "filename", help="filename to write modlist to or specify '-' for stdout"
+        )
         args = parser.parse_args(sys.argv[2:])
-
         mods = Manager(self.path).get_mods_list()
-        if (args.filename != "-"):
+        if args.filename != "-":
             ModList.write_text_modlist(mods, args.filename)
             print("Mod list written to {}.\n".format(args.filename))
         else:
             print(ModList.export_text_modlist(mods))
 
     def list(self):
-        if (not (s := Manager(self.path).get_mod_table())):
+        if not (s := Manager(self.path).get_mod_table()):
             print("No mods installed. Add them using the 'sync' command.")
         print(s)
 
     def sync(self):
         parser = argparse.ArgumentParser(description="Syncs a mod from the workshop")
         parser.add_argument("modname", help="mod or modlist to sync")
-        parser.add_argument("-f", "--file", action="store_true", help="specify modlist instead of modname")
+        parser.add_argument(
+            "-f",
+            "--file",
+            action="store_true",
+            help="specify modlist instead of modname",
+        )
         args = parser.parse_args(sys.argv[2:])
 
         if args.file:
@@ -246,53 +285,70 @@ The available commands are:
             results = WorkshopWebScraper.workshop_search(args.modname)
             for n, element in enumerate(reversed(results)):
                 n = abs(n - len(results))
-                print("{}. {} {}".format(n,element[WorkshopResultsEnum.TITLE.value],element[WorkshopResultsEnum.AUTHOR.value]))
+                print(
+                    "{}. {} {}".format(
+                        n,
+                        element[WorkshopResultsEnum.TITLE.value],
+                        element[WorkshopResultsEnum.AUTHOR.value],
+                    )
+                )
             print("Packages to install (eg: 1 2 3, 1-3 or ^4)")
 
-
-            while(True):
+            while True:
                 try:
                     selection = int(input()) - 1
-                    if (selection >= len(results) or selection < 0):
-                        raise InvalidSelectionException('Out of bounds')
+                    if selection >= len(results) or selection < 0:
+                        raise InvalidSelectionException("Out of bounds")
                     break
                 except ValueError:
                     print("Must enter valid integer")
                 except InvalidSelectionException:
-                    print('Selection out of bounds.')
+                    print("Selection out of bounds.")
 
+            print(
+                "Package(s): {} will be installed. Continue? [y/n] ".format(
+                    results[selection][WorkshopResultsEnum.TITLE.value]
+                ),
+                end="",
+            )
 
-            print("Package(s): {} will be installed. Continue? [y/n] ".format(results[selection][WorkshopResultsEnum.TITLE.value]), end='')
-
-            if (input() != "y"):
+            if input() != "y":
                 return False
 
-            Manager(self.path).sync_mod(results[selection][WorkshopResultsEnum.STEAMID.value])
-           
+            Manager(self.path).sync_mod(
+                results[selection][WorkshopResultsEnum.STEAMID.value]
+            )
 
     def update(self):
         parser = argparse.ArgumentParser(description="Updates all mods in directory")
         parser.add_argument("filename", nargs="?", const=1, default=self.path)
         args = parser.parse_args(sys.argv[2:])
 
-        print("Preparing to update following packages: " + 
-              ", ".join(str(x) for x in Manager(self.path).get_mods_names()) +
-              "\n\nWould you like to continue? [y/n]")
-        
-        if (input() != "y"):
+        print(
+            "Preparing to update following packages: "
+            + ", ".join(str(x) for x in Manager(self.path).get_mods_names())
+            + "\n\nWould you like to continue? [y/n]"
+        )
+
+        if input() != "y":
             return False
 
         Manager(self.path).update_all_mods(args.filename)
         print("Package update complete.")
 
     def backup(self):
-        parser = argparse.ArgumentParser(description="Creates a backup of the mod directory state.")
-        parser.add_argument("filename", nargs="?", const=1, default="/tmp/rimworld.tar.bz2")
+        parser = argparse.ArgumentParser(
+            description="Creates a backup of the mod directory state."
+        )
+        parser.add_argument(
+            "filename", nargs="?", const=1, default="/tmp/rimworld.tar.bz2"
+        )
         args = parser.parse_args(sys.argv[2:])
 
         print("Backing up mod directory to '{}.\n".format(args.filename))
         Manager(self.path).backup_mod_dir(args.filename)
         print("Backup completed to " + args.filename + ".")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     CLI()
