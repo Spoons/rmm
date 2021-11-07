2e0d7f88e3e4b39b0469f27a72b32677c69fe76d
spoons <childofunix@gmail.com>
2021-11-07 18:16:59 -0500

    feat: completed workshop detail scraper

--- src/rmm/core2.py
+++ src/rmm/core2.py
@@ -1,66 +1,65 @@
 #!/bin/python3
-"""
-RimWorld Mod Manager
-
-Usage:
-  rmm backup <file>
-  rmm export [options] <file>
-  rmm import [options] <file>
-  rmm list [options]
-  rmm migrate [options]
-  rmm query [options] [<term>...]
-  rmm remove [options] [<term>...]
-  rmm search <term>...
-  rmm sync [options] [sync options] <name>...
-  rmm update [options] [sync options]
-  rmm -h | --help
-  rmm -v | --version
-
-Operations:
-  backup            Backups your mod directory to a tar, gzip,
-                      bz2, or xz archive. Type inferred by name.
-  export            Save mod list to file.
-  import            Install a mod list from a file.
-  list              List installed mods.
-  migrate           Remove mods from workshop and install locally.
-  query             Search installed mods.
-  remove            Remove installed mod.
-  search            Search Workshop.
-  sync              Install or update a mod.
-  update            Update all mods from Steam.
-
-Parameters
-  term              Name, author, steamid
-  file              File path
-  name              Name of mod.
-
-Sync Options:
-  -f --force        Force mod directory overwrite
-
-Options:
-  -p --path DIR     RimWorld path.
-  -w --workshop DIR Workshop Path.
-"""
-
 from __future__ import annotations
-from typing import Iterable, cast, Optional, Type, Generator, Any, Iterator
-import sys
-from collections.abc import Sequence,MutableSequence
-from abc import ABC, abstractmethod
-import xml.etree.ElementTree as ET
-import tempfile
-from pathlib import Path
-import docopt
+
+import csv
+import re
 import urllib.request
+import xml.etree.ElementTree as ET
+from abc import ABC, abstractmethod
+from collections.abc import MutableSequence
 from multiprocessing import Pool
-import networkx as nx
-import matplotlib.pyplot as plt
-from enum import Enum
-import io
-from rmm.utils.processes import run_sh
+from pathlib import Path
+from typing import Any, Generator, Iterable, Iterator, Optional, cast
+import time
 
 from bs4 import BeautifulSoup
-import csv
+
+from utils.processes import run_sh
+
+
+class Useage:
+    """
+    RimWorld Mod Manager
+
+    Usage:
+    rmm backup <file>
+    rmm export [options] <file>
+    rmm import [options] <file>
+    rmm list [options]
+    rmm migrate [options]
+    rmm query [options] [<term>...]
+    rmm remove [options] [<term>...]
+    rmm search <term>...
+    rmm sync [options] [sync options] <name>...
+    rmm update [options] [sync options]
+    rmm -h | --help
+    rmm -v | --version
+
+    Operations:
+    backup            Backups your mod directory to a tar, gzip,
+                        bz2, or xz archive. Type inferred by name.
+    export            Save mod list to file.
+    import            Install a mod list from a file.
+    list              List installed mods.
+    migrate           Remove mods from workshop and install locally.
+    query             Search installed mods.
+    remove            Remove installed mod.
+    search            Search Workshop.
+    sync              Install or update a mod.
+    update            Update all mods from Steam.
+
+    Parameters
+    term              Name, author, steamid
+    file              File path
+    name              Name of mod.
+
+    Sync Options:
+    -f --force        Force mod directory overwrite
+
+    Options:
+    -p --path DIR     RimWorld path.
+    -w --workshop DIR Workshop Path.
+    """
 
 
 class Mod:
@@ -197,7 +196,7 @@ class ModList(MutableSequence):
 
 class ModFolderReader:
     @staticmethod
-    def create_mods_list(path) -> MutableSequence[Mod]:
+    def create_mods_list(path) -> ModList[Mod]:
         with Pool(16) as p:
             mods = filter(
                 None,
@@ -221,7 +220,7 @@ class ModListSerializer(ABC):
         pass
 
 
-class CsvStringBuilder():
+class CsvStringBuilder:
     def __init__(self):
         self.value = []
 
@@ -229,13 +228,13 @@ class CsvStringBuilder():
         self.value.append(row)
 
     def pop(self) -> None:
-        return(self.value.pop())
+        return self.value.pop()
 
     def __iter__(self) -> Iterator[str]:
         return iter(self.value)
 
 
-class ModLisCSVFormat(ModListSerializer):
+class ModListCSVFormat(ModListSerializer):
     HEADER = {"PACKAGE_ID": 0, "STEAM_ID": 1, "REPO_URL": 2}
 
     @classmethod
@@ -254,7 +253,7 @@ class ModLisCSVFormat(ModListSerializer):
     @classmethod
     def serialize(cls, mods: MutableSequence) -> Generator[str, None, None]:
         buffer = CsvStringBuilder()
-        writer = csv.writer(cast(Any,buffer))
+        writer = csv.writer(cast(Any, buffer))
         for m in mods:
             writer.writerow(cls.format(m))
             yield buffer.pop().strip()
@@ -271,7 +270,7 @@ class ModLisCSVFormat(ModListSerializer):
         )
 
 
-class ModListFileFormat(ModListSerializer):
+class ModListV2Format(ModListSerializer):
     MAGIC_ID = "rmm_modlist_v2"
     SEPERATOR = "::"
     PACKAGE_ID = 0
@@ -309,8 +308,9 @@ class ModListFileFormat(ModListSerializer):
         )
 
 
-class ModListV1FileFormat(ModListSerializer):
-    STEAM_ID=0
+class ModListV1Format(ModListSerializer):
+    STEAM_ID = 0
+
     @classmethod
     def parse(cls, text: str) -> Generator[Mod, None, None]:
         for line in text:
@@ -332,7 +332,7 @@ class ModListV1FileFormat(ModListSerializer):
         return "{} # {} by {} ".format(str(mod.steamid), mod.name, mod.author)
 
 
-class ModListReader:
+class ModListStreamer:
     @staticmethod
     def read(path: Path, serializer: ModListSerializer) -> Optional[MutableSequence]:
         try:
@@ -342,13 +342,13 @@ class ModListReader:
             print(e)
             return None
 
-        return [m for m in ModListFileFormat.parse(text)]
+        return [m for m in ModListV2Format.parse(text)]
 
     @staticmethod
     def write(path: Path, mods: MutableSequence, serializer: ModListSerializer):
         try:
             with path.open("w+") as f:
-                [f.write(line+"\n") for line in serializer.serialize(mods)]
+                [f.write(line + "\n") for line in serializer.serialize(mods)]
         except OSError as e:
             print(e)
             return False
@@ -356,49 +356,202 @@ class ModListReader:
 
 
 class SteamDownloader:
-    @staticmethod
-    def download(mods: MutableSequence, folder: Path):
+    def __init__(self, folder: Path):
+        self.home_path = folder
+        self.mod_path = folder / ".steam/steamapps/workshop/content/294100/"
+
+    def _get(self, mods: MutableSequence):
         def workshop_format(mods):
             return (s := " +workshop_download_item 294100 ") + s.join(
                 m.steamid for m in mods if not None
             )
 
         query = 'env HOME="{}" steamcmd +login anonymous "{}" +quit >&2'.format(
-            str(folder), workshop_format(mods)
+            str(self.home_path), workshop_format(mods)
         )
-        run_sh(query)
+        return run_sh(query)
+
+    def download(self, mods: MutableSequence[Mod]) -> ModList:
+        self._get(mods)
+        return ModFolderReader.create_mods_list(self.home_path)
+
+
+class WorkshopResult:
+    def __init__(
+        self,
+        steamid,
+        name=None,
+        author=None,
+        description=None,
+        update_time=None,
+        size=None,
+        num_rating=None,
+        rating=None,
+        create_time=None,
+        num_ratings=None,
+    ):
+        self.steamid = steamid
+        self.name = name
+        self.author = author
+        self.description = description
+        self.update_time = update_time
+        self.size = size
+        self.create_time = create_time
+        self.num_ratings = num_ratings
+        self.rating = rating
+
+    def __str__(self):
+        return "\n".join([prop + ": " + str(getattr(self,prop)) for prop in self.__dict__ if not callable(self.__dict__[prop])])
+
+    def __repr__(self):
+        return(self.__str__())
+
+    def __eq__(self, other: WorkshopResult) -> bool:
+        if not isinstance(other, WorkshopResult):
+            raise NotImplementedError
+        return self.steamid == other.steamid
+
+    def _merge(self, other: WorkshopResult):
+        if not isinstance(other, WorkshopResult):
+            raise NotImplementedError
+        for prop in other.__dict__:
+            if not callable(other.__dict__[prop]) and hasattr(self, prop) and getattr(other, prop):
+                setattr(self, prop, (getattr(other, prop)))
+
+    def get_details(self):
+        self._merge(WorkshopWebScraper.detail(self.steamid))
+        return self
+
+
+class WorkshopWebScraper:
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
+    }
+    index_query = (
+        "https://steamcommunity.com/workshop/browse/?appid=294100&searchtext={}"
+    )
+    detail_query = "https://steamcommunity.com/sharedfiles/filedetails/?id={}"
+
+    @classmethod
+    def _request(cls, url: str, term: str):
+        return urllib.request.urlopen(
+            urllib.request.Request(
+                url.format(term.replace(" ", "+")),
+                headers=WorkshopWebScraper.headers,
+            )
+        )
+
+    @classmethod
+    def detail(cls, steamid: int):
+        results = BeautifulSoup(
+            cls._request(cls.detail_query, str(steamid)),
+            "html.parser",
+        )
+
+        details = results.find_all("div", class_="detailsStatRight")
+        try:
+            size = details[0].get_text()
+        except IndexError:
+            size = None
+        try:
+            created = details[1].get_text()
+        except IndexError:
+            created = None
+        try:
+            updated = details[2].get_text()
+        except IndexError:
+            updated = None
+        try:
+            description = results.find("div", class_="workshopItemDescription").get_text()
+        except AttributeError:
+            description = None
+        try:
+            num_ratings = results.find("div", class_="numRatings").get_text()
+        except AttributeError:
+            num_ratings = None
+        try:
+            rating = re.search(
+                "([1-5])(?:-star)", str(results.find("div", class_="fileRatingDetails").img)
+            ).group(1)
+        except AttributeError:
+            rating = None
+
+        return WorkshopResult(
+            steamid,
+            size=size,
+            create_time=created,
+            update_time=updated,
+            description=description,
+            num_rating=num_ratings,
+            rating=rating,
+        )
+
+    @classmethod
+    def search(cls, term: str) -> Generator[WorkshopResult, None, None]:
+        results = BeautifulSoup(
+            cls._request(cls.index_query, term),
+            "html.parser",
+        ).find_all("div", class_="workshopItem")
+
+        for r in results:
+            try:
+                item_title = r.find("div", class_="workshopItemTitle").get_text()
+                author_name = r.find("div", class_="workshopItemAuthorName").get_text()
+                steamid = int(
+                    re.search(r"\d+", r.find("a", class_="ugc")["href"]).group()
+                )
+            except (AttributeError, ValueError):
+                continue
+            yield WorkshopResult(steamid, name=item_title, author=author_name)
+
+
+class Sort:
+    @staticmethod
+    def graph():
+        DG = nx.DiGraph()
+
+        ignore = ["brrainz.harmony", "UnlimitedHugs.HugsLib"]
+        for m in mods:
+            if m.after:
+                for a in m.after:
+                    if a in mods:
+                        if not a in ignore and not m.packageid in ignore:
+                            DG.add_edge(a, m.packageid)
+            if m.before:
+                for b in m.before:
+                    if b in mods:
+                        if not b in ignore and not m.packageid in ignore:
+                            DG.add_edge(m.packageid, b)
+
+        pos = nx.spring_layout(DG, seed=56327, k=0.8, iterations=15)
+        nx.draw(
+            DG,
+            pos,
+            node_size=100,
+            alpha=0.8,
+            edge_color="r",
+            font_size=8,
+            with_labels=True,
+        )
+        ax = plt.gca()
+        ax.margins(0.08)
+
+        print("topological sort:")
+        sorted = list(nx.topological_sort(DG))
+        for n in sorted:
+            print(n)
+
+        plt.show()
 
 
 if __name__ == "__main__":
+    # Create test mod list
     mods = ModFolderReader.create_mods_list(
         Path("/tmp/rmm/.steam/steamapps/workshop/content/294100/")
     )
-    ModListReader.write(Path("/tmp/test_modlist"), mods, ModLisCSVFormat())
-    # DG = nx.DiGraph()
-
-    # ignore = ["brrainz.harmony", "UnlimitedHugs.HugsLib"]
-    # for m in mods:
-    #     if m.after:
-    #         for a in m.after:
-    #             if a in mods:
-    #                 if not a in ignore and not m.packageid in ignore:
-    #                     DG.add_edge(a, m.packageid)
-    #     if m.before:
-    #         for b in m.before:
-    #             if b in mods:
-    #                 if not b in ignore and not m.packageid in ignore:
-    #                     DG.add_edge(m.packageid, b)
-
-    # pos = nx.spring_layout(DG, seed=56327, k=0.8, iterations=15)
-    # nx.draw(
-    #     DG, pos, node_size=100, alpha=0.8, edge_color="r", font_size=8, with_labels=True
-    # )
-    # ax = plt.gca()
-    # ax.margins(0.08)
-
-    # print("topological sort:")
-    # sorted = list(nx.topological_sort(DG))
-    # for n in sorted:
-    #     print(n)
-
-    # # plt.show()
+    ModListStreamer.write(Path("/tmp/test_modlist"), mods, ModListCSVFormat())
+    # Workshop scrape test
+    results = WorkshopWebScraper.search("rimhud")
+    for n in results:
+        print(str(n.get_details())+"\n")
+        time.sleep(1)
