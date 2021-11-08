cc7ba9701693e42d27b3244a082bbb2f8755f69d
spoons <childofunix@gmail.com>
2021-11-07 22:40:14 -0500

    feat: use pbr for package management
    improved help output
    added MacOS default path

--- src/rmm/core.py
+++ src/rmm/core.py
@@ -40,28 +40,33 @@ Options:
 """
 
 from __future__ import annotations
+
+import importlib.metadata
 import os
 import sys
-from typing import Iterable, cast, Optional
-import xml.etree.ElementTree as ET
-import argparse
 import tempfile
-import pathlib
-import docopt
 import urllib.request
-from multiprocessing import Pool
-
+import xml.etree.ElementTree as ET
 from enum import Enum
+from multiprocessing import Pool
+from typing import Iterable, Optional, cast
+
+import docopt
 from bs4 import BeautifulSoup
+
 from rmm.utils.processes import execute, run_sh
 
+__version__ = importlib.metadata.version('rmm-spoons')
 
-RMM_VERSION = "0.0.6"
 
 DEFAULT_GAME_PATHS = [
     "~/GOG Games/RimWorld",
     "~/.local/share/Steam/steamapps/common/RimWorld",
+    "/Applications/Rimworld.app/Mods",
+    "~/Library/Application Support/Steam/steamapps/common/RimWorld"
 ]
+
+DEFAULT_CONFIG_PATH = ["~/Library/Application\ Support/Rimworld/"]
 
 
 class InvalidSelectionException(Exception):
@@ -452,7 +457,7 @@ class CLI:
         self.workshop_path = None
 
         try:
-            arguments = docopt.docopt(__doc__, version="RMM 0.0.4", more_magic=True)
+            arguments = docopt.docopt(__doc__, version=f"RMM {__version__}", more_magic=True)
         except docopt.DocoptExit:
             arguments = {}
             print(__doc__)
@@ -461,6 +466,10 @@ class CLI:
                 sys.exit(1)
             if len(sys.argv) > 0:
                 sys.exit(0)
+
+        if arguments['-v']:
+            print(f"RMM {__version__}")
+            sys.exit(0)
 
         def check_default_paths():
             for path in DEFAULT_GAME_PATHS:
@@ -495,7 +504,8 @@ class CLI:
             self.path = check_default_paths()
 
         if not self.path:
-            print("Could not find RimWorld. Exiting...")
+            print("Could not find RimWorld. Exiting...",
+                  "\nSet the path to the game using -p or the RMM_PATH environment variable.")
             exit(1)
 
         print("rimworld path: {}".format(self.path))
@@ -752,15 +762,6 @@ class CLI:
                     element.author,
                 )
             )
-
-    def version(self):
-        version_string = f"""rmm {RMM_VERSION}
-Copyright (C) 2021 Michael Ciociola
-License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.
-This is free software: you are free to change and redistribute it.
-There is NO WARRANTY, to the extent permitted by law."""
-        print(version_string)
-
 
 def run():
     t = CLI()
