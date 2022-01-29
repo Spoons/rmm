01e0d4ae8d1eb2dcd57741e22e85fa24b6c0e5e2
Michael Ciociola <dev@ciocio.la>
2022-01-28 22:30:05 -0500

    fix: improve performance with dict lookups

--- src/rmm/manager.py
+++ src/rmm/manager.py
@@ -67,15 +67,18 @@ class Manager:
                 mod = Mod.create_from_workshorp_result(mod)
             self.remove_mod(mod)
 
-    def sync_mods(self, queue):
-        (_, steam_cache_path) = SteamDownloader.download(
+    def sync_mods(self, queue: list[Mod] | list[WorkshopResult]):
+        steam_mods, steam_cache_path = SteamDownloader.download(
             [mod.steamid for mod in queue if mod.steamid]
         )
-        # game_dir_mods = ModFolder.read(config.game_path)
 
         for mod in queue:
             if isinstance(mod, WorkshopResult):
-                mod = Mod.create_from_workshorp_result(mod)
+                new_mod = [m for m in steam_mods if m.steamid == mod.steamid]
+                if len(new_mod) == 1:
+                    mod = new_mod[0]
+                else:
+                    mod = Mod(steamid = mod.steamid)
             if not isinstance(mod.steamid, int):
                 continue
             success = False
@@ -91,31 +94,49 @@ class Manager:
                 print(f"Installed {mod.title()}")
 
     def _mod_config_state(self, mods):
+        return [m for _, m in self._mod_config_state_dict(mods).items()]
+
+    def _mod_config_state_dict(self, mods):
         if self.modsconfig:
-            enabled_mods = self.enabled_mods()
-            for n in mods:
-                if n in enabled_mods:
-                    n.enabled = True
+            enabled_mods = self._enabled_mod_pids()
+            for k, v in mods.items():
+                if k in enabled_mods:
+                    v.enabled = True
                 else:
-                    n.enabled = False
+                    v.enabled = False
         return mods
+
     def installed_mods(self):
-        mods = ModFolder.read(self.config.mod_path)
+        mods = ModFolder.read_dict(self.config.mod_path)
         return self._mod_config_state(mods)
+
+    def installed_mods_dict(self):
+        mods = ModFolder.read_dict(self.config.mod_path)
+        return self._mod_config_state_dict(mods)
 
     def search_installed(self, term):
-        mods = ModFolder.search(self.config.mod_path, term)
+        mods = ModFolder.search_dict(self.config.mod_path, term)
         return self._mod_config_state(mods)
 
+    def _enabled_mod_pids(self):
+        return [ k for k in self.modsconfig.mods ]
+
     def enabled_mods(self):
-        return self.modsconfig.mods
+        installed_mods = self.installed_mods_dict()
+        l = list()
+        for n in self.modsconfig.mods:
+            try:
+                l.append(installed_mods[n])
+            except KeyError:
+                continue
+        return l
 
     def disabled_mods(self):
-        enabled_mods = self.enabled_mods()
+        enabled_mods = self._enabled_mod_pids()
         installed_mods = self.installed_mods()
         return util.list_loop_exclusion(installed_mods, enabled_mods)
 
-    def _enable_mod(self, mod: str|Mod):
+    def _enable_mod(self, mod: str | Mod):
         if isinstance(mod, str):
             mod = Mod(packageid=mod)
         if not mod.packageid:
@@ -129,8 +150,7 @@ class Manager:
         print("Updating ModsConfig.xml")
         self.modsconfig.write()
 
-
-    def _disable_mod(self, mod: str|Mod):
+    def _disable_mod(self, mod: str | Mod):
         if isinstance(mod, str):
             mod = Mod(packageid=mod)
         if not mod.packageid:
@@ -151,12 +171,12 @@ class Manager:
         self.modsconfig.autosort(self.installed_mods(), self.config)
 
     def order_mods(self):
-        enabled_mods = self.enabled_mods()
+        enabled_mods = self._enabled_mod_pids()
         installed_mods = self.installed_mods()
 
         sorted_mods = []
         for m in enabled_mods:
-            for j,im in enumerate(installed_mods + EXPANSION_PACKAGES):
+            for j, im in enumerate(installed_mods + EXPANSION_PACKAGES):
                 if m == im:
                     sorted_mods.append(im)
                     break
